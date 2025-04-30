705e2e637bad4f930a9e6d4d489cbf42010e7708
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-30 17:01:02 +0900

    feat: 컬러셋 추가

---
+++ lib/core/theme/luckify_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LuckifyColors {
+  static const Color primary = Color(0xFF38105A);
+  static const Color primaryLight = Color(0xFFEBDDF8);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFF8E8E93);
+  static const Color black = Color(0xFF1C1C1E);
+}
