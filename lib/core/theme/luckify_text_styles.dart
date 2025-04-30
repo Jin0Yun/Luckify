bb2f449bc9a092a4c4378bf09d27bed225ef5c58
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-05-01 00:05:50 +0900

    refactor: 중복 TextStyle 제거 및 _base 스타일 적용

--- lib/core/theme/luckify_text_styles.dart
+++ lib/core/theme/luckify_text_styles.dart
@@ -2,36 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:luckify/core/theme/luckify_colors.dart';
 
 class LuckifyTextStyles {
-  static const fortuneTitle = TextStyle(
+  static const _base = TextStyle(
     fontFamily: 'Moneygraphy',
+    color: LuckifyColors.black,
+  );
+
+  static final fortuneTitle = _base.copyWith(
     fontWeight: FontWeight.w700,
     fontSize: 23,
     color: LuckifyColors.primary,
   );
 
-  static const fortuneHint = TextStyle(
-    fontFamily: 'Moneygraphy',
+  static final fortuneHint = _base.copyWith(
     fontWeight: FontWeight.w200,
     fontSize: 13,
     color: LuckifyColors.grey,
   );
 
-  static const buttonText = TextStyle(
-    fontFamily: 'Moneygraphy',
+  static final buttonText = _base.copyWith(
     fontWeight: FontWeight.w500,
     fontSize: 16,
-    color: LuckifyColors.black,
   );
 
-  static const appBarTitle = TextStyle(
-    fontFamily: 'Moneygraphy',
+  static final appBarTitle = _base.copyWith(
     fontWeight: FontWeight.w500,
     fontSize: 18,
-    color: LuckifyColors.black,
   );
 
-  static const chatInputHint = TextStyle(
-    fontFamily: 'Moneygraphy',
+  static final chatInputHint = _base.copyWith(
     fontWeight: FontWeight.w300,
     fontSize: 14,
     color: LuckifyColors.grey,
