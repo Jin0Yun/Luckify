beed44ec2a2e0de7e408d93a0b2963669adfb707
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-05-10 01:31:01 +0900

    refactor: ResponseMapper의 의존성 MessageMapper -> ChoiceMapper로 변경
    - 중복 코드 제거

--- lib/data/mapper/response_mapper.dart
+++ lib/data/mapper/response_mapper.dart
@@ -1,54 +1,32 @@
-import 'package:luckify/data/dto/choice_dto.dart';
 import 'package:luckify/data/dto/response_dto.dart';
 import 'package:luckify/data/mapper/base_mapper.dart';
-import 'package:luckify/data/mapper/message_mapper.dart';
+import 'package:luckify/data/mapper/choice_mapper.dart';
 import 'package:luckify/domain/entity/response_entity.dart';
-import 'package:luckify/domain/entity/choice_entity.dart';
 
 class ResponseMapper implements BaseMapper<ResponseDTO, ResponseEntity> {
-  final MessageMapper _messageMapper;
+  final ChoiceMapper _choiceMapper;
 
-  ResponseMapper(this._messageMapper);
+  ResponseMapper(this._choiceMapper);
 
   @override
   ResponseDTO toDTO(ResponseEntity entity) {
-    final choiceDTOs =
-        entity.choices
-            .map(
-              (choice) => ChoiceDTO(
-                index: choice.index,
-                message: _messageMapper.toDTO(choice.message),
-              ),
-            )
-            .toList();
-
     return ResponseDTO(
       id: entity.id,
       object: entity.object,
       created: entity.created.millisecondsSinceEpoch ~/ 1000,
       model: entity.model,
-      choices: choiceDTOs,
+      choices: entity.choices.map((choice) => _choiceMapper.toDTO(choice)).toList(),
     );
   }
 
   @override
   ResponseEntity toEntity(ResponseDTO dto) {
-    final choiceEntities =
-        dto.choices
-            .map(
-              (choice) => ChoiceEntity(
-                index: choice.index,
-                message: _messageMapper.toEntity(choice.message),
-              ),
-            )
-            .toList();
-
     return ResponseEntity(
       id: dto.id,
       object: dto.object,
       created: DateTime.fromMillisecondsSinceEpoch(dto.created * 1000),
       model: dto.model,
-      choices: choiceEntities,
+      choices: dto.choices.map((choice) => _choiceMapper.toEntity(choice)).toList(),
     );
   }
 }
