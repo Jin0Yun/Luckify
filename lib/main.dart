e1a8b5ff520bb45bb8b842483e27e1325cfd7e1e
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-05-10 16:00:58 +0900

    feat: DI 프로바이더 설정
    - 전체 레이어 의존성 주입 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:luckify/presentation/screen/home_screen.dart';
 import 'package:luckify/core/theme/luckify_colors.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
