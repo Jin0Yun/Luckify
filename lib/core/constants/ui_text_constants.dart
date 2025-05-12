32753f4a6047a2352d5130e3adfd0b5ba441deca
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-05-12 23:33:22 +0900

    feat: 상수 클래스 추가

---
+++ lib/core/constants/ui_text_constants.dart
@@ -0,0 +1,5 @@
+class UITextConstants {
+  static const String homeScreenTitle = '특별한 운세가 필요하신가요?';
+  static const String homeScreenSubtitle = '원하는 운세를 선택해보세요.';
+  static const String selectButtonText = '선택하기';
+}
