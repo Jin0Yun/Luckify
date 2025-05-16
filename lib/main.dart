4046ab9820378b7934824e1a2bd4ed87b4e84098
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-05-16 15:12:38 +0900

    feat: 로그인 상태에 따른 화면 전환 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,38 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:luckify/firebase_options.dart';
 import 'package:luckify/presentation/screen/home_screen.dart';
 import 'package:luckify/core/theme/luckify_colors.dart';
+import 'package:luckify/presentation/screen/login_screen.dart';
+import 'config/di/viewmodel_providers.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await dotenv.load(fileName: ".env");
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authState = ref.watch(authViewModelProvider);
+
     return MaterialApp(
       theme: ThemeData(
         scaffoldBackgroundColor: LuckifyColors.white,
         primaryColor: LuckifyColors.primary,
         fontFamily: 'Moneygraphy',
       ),
-      home: const HomeScreen(),
+      home: authState.isLoggedIn ? const HomeScreen() : const LoginScreen(),
     );
   }
 }
