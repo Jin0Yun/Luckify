d0ccc2a80319f1fe04ed034e6fe595a21e46adf8
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-05-16 14:59:33 +0900

    feat: 구글 로그인 인증 로직 구현

---
+++ lib/domain/repository/auth_repository.dart
@@ -0,0 +1,12 @@
+import 'package:luckify/domain/entity/user_entity.dart';
+
+enum AuthProvider {
+  google,
+}
+
+abstract class AuthRepository {
+  Future<UserEntity> signInWithProvider(AuthProvider provider);
+  Future<void> signOut();
+  Stream<UserEntity> get authStateChanges;
+  UserEntity get currentUser;
+}
